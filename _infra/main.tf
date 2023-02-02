135507941f5df2dbaba35353caed375b8b6d39cf
Andre Silva <andreswebs@pm.me>
2023-02-02 13:17:20 -0300

    update

--- _infra/main.tf
+++ _infra/main.tf
@@ -1,23 +1,23 @@
-module "jenkins" {
-  source                       = "../jenkins/terraform"
-  external_access_ip_whitelist = var.external_access_ip_whitelist
-  snapshot_name                = "jenkins-data-manual-2022-12-08"
-}
+# module "jenkins" {
+#   source                       = "../jenkins/terraform"
+#   external_access_ip_whitelist = var.external_access_ip_whitelist
+#   snapshot_name                = "jenkins-data-manual-2023-02-01"
+# }
 
-module "drone" {
-  source                       = "../drone/terraform"
-  external_access_ip_whitelist = var.external_access_ip_whitelist
-}
+# module "drone" {
+#   source                       = "../drone/terraform"
+#   external_access_ip_whitelist = var.external_access_ip_whitelist
+# }
 
-module "peertube" {
-  source                       = "../peertube/terraform"
-  external_access_ip_whitelist = var.external_access_ip_whitelist
-}
+# module "peertube" {
+#   source                       = "../peertube/terraform"
+#   external_access_ip_whitelist = var.external_access_ip_whitelist
+# }
 
-module "vaultwarden" {
-  source                       = "../vaultwarden/terraform"
-  external_access_ip_whitelist = var.external_access_ip_whitelist
-}
+# module "vaultwarden" {
+#   source                       = "../vaultwarden/terraform"
+#   external_access_ip_whitelist = var.external_access_ip_whitelist
+# }
 
 # module "whoami" {
 #   source                       = "../whoami/terraform"
