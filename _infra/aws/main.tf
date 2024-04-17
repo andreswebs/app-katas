85569ec04f0d8e5fbc501b8fcf9923372b1fe654
Andre Silva <andreswebs@pm.me>
2024-04-17 07:56:39 -0300

    rm unused

--- _infra/aws/main.tf
+++ _infra/aws/main.tf
@@ -1,7 +1,7 @@
-# module "ec2_base_01" {
+# module "ec2_base" {
 #   source         = "../modules/aws-ec2-base/"
-#   vpc_id         = var.vpc_id_01
-#   cidr_whitelist = var.cidr_whitelist_01
+#   vpc_id         = var.vpc_id
+#   cidr_whitelist = var.cidr_whitelist
 #   name           = "k3s"
 
 #   allow_web_traffic = true
@@ -15,45 +15,15 @@
 
 # }
 
-# module "ec2_instance_01" {
+# module "ec2_instance" {
 #   source                 = "../modules/aws-ec2-instance-linux/"
-#   subnet_id              = var.subnet_id_01
-#   vpc_security_group_ids = [module.ec2_base_01.security_group.id]
-#   ssh_key_name           = module.ec2_base_01.key_pair.key_name
-#   iam_profile_name       = module.ec2_base_01.instance_profile.name
+#   subnet_id              = var.subnet_id
+#   vpc_security_group_ids = [module.ec2_base.security_group.id]
+#   ssh_key_name           = module.ec2_base.key_pair.key_name
+#   iam_profile_name       = module.ec2_base.instance_profile.name
 #   name                   = "k3s"
 # }
 
-# output "ec2_instance_01_public_ip" {
-#   value = module.ec2_instance_01.public_ip
+# output "ec2_instance_public_ip" {
+#   value = module.ec2_instance.public_ip
 # }
-
-module "ec2_base_02" {
-  source         = "../modules/aws-ec2-base/"
-  vpc_id         = var.vpc_id_02
-  cidr_whitelist = var.cidr_whitelist_02
-  name           = "data-poc"
-
-  extra_whitelisted_ingress_rules = [
-    {
-      from_port = "3389"
-      to_port   = "3389"
-    }
-  ]
-
-}
-
-module "ec2_instance_02" {
-  source                 = "../modules/aws-ec2-instance-linux/"
-  subnet_id              = var.subnet_id_02
-  vpc_security_group_ids = [module.ec2_base_02.security_group.id]
-  ssh_key_name           = module.ec2_base_02.key_pair.key_name
-  iam_profile_name       = module.ec2_base_02.instance_profile.name
-  instance_type          = "m6a.2xlarge"
-  name                   = "data-poc"
-  volume_size            = 100
-}
-
-output "ec2_instance_02_public_ip" {
-  value = module.ec2_instance_02.public_ip
-}
--- _infra/aws/variables.tf
+++ _infra/aws/variables.tf
@@ -1,35 +1,11 @@
-# variable "cidr_whitelist" {
-#   type = list(string)
-# }
-
-# variable "vpc_id" {
-#   type = string
-# }
-
-# variable "subnet_id" {
-#   type = string
-# }
-
-variable "cidr_whitelist_01" {
+variable "cidr_whitelist" {
   type = list(string)
 }
 
-variable "vpc_id_01" {
+variable "vpc_id" {
   type = string
 }
 
-variable "subnet_id_01" {
-  type = string
-}
-
-variable "cidr_whitelist_02" {
-  type = list(string)
-}
-
-variable "vpc_id_02" {
-  type = string
-}
-
-variable "subnet_id_02" {
+variable "subnet_id" {
   type = string
 }
